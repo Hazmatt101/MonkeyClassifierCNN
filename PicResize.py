d9611d1caeaff660c975d825f3689207456b37d5
andrewdieken <dieke102@mail.chapman.edu>
2019-01-21 20:34:18 -0800

    update pic resize

--- PicResize.py
+++ PicResize.py
@@ -3,14 +3,19 @@ from resizeimage import resizeimage
 import os
 import re
 
-directory = os.fsencode("./10-monkey-species/training/n0/")
-for file in os.listdir(directory):
-    filename = os.fsdecode(file)
-    print(filename)
+for i in range(0, 10):
+    directory = os.fsencode("./10-monkey-species/validation/n{}/".format(i))
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
 
-    with open(filename, 'r+b') as f:
-        with Image.open(f) as image:
-            cover = resizeimage.resize_cover(image, [400, 300])
-            filename = re.sub('.jpg', '', filename)
-            filename = filename + '-resized.jpg'
-            cover.save(filename, image.format)
+        if filename != '.DS_Store':
+            open_temp_path = ("./10-monkey-species/validation/n{}/".format(i)) + filename
+            print(open_temp_path)
+
+            with open(open_temp_path, 'r+b') as f:
+                with Image.open(f) as image:
+                    cover = resizeimage.resize_cover(image, [300, 300])
+                    filename = re.sub('.jpg', '', filename)
+                    filename = filename + '-resized.jpg'
+                    write_temp_path = ("./validation-resized/n{}/".format(i)) + filename
+                    cover.save(write_temp_path, image.format)
